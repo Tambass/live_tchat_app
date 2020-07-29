0cf241f7e597acf9ffc0e27b48578addc47004da
Tambass <tambass85@gmail.com>
2020-07-29 16:51:54 +0200

    verification de connection en cour

--- js/client.js
+++ js/client.js
@@ -24,30 +24,6 @@ const append = (message, position) => {
 // const name = prompt("Entrez votre nom pour rejoindre le t'chat");
 // socket.emit("new-user-joined", name);
 
-// Si un nouvel utilisateur rejoint le t'chat, on informe le server de son nom
-socket.on("user-joined", (name) => {
-  append(`${name} a rejoint le t'chat`, "right");
-});
-
-// Si le server envoi un message, on le reçoi
-socket.on("receive", (data) => {
-  append(`${data.name}: ${data.message}`, "left");
-});
-
-// Si un utilisateur quitte le t'chat, on informe les autres utilisateurs
-socket.on("left", (name) => {
-  append(`${name} a quitté le t'chat`, "left");
-});
-
-// Si le formulaire est soumi, on envoi le message au server
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const message = messageInput.value;
-  append(`Toi: ${message}`, "right");
-  socket.emit("send", message);
-  messageInput.value = "";
-});
-
 // Custom PROMPT
 
 function testit() {
@@ -95,3 +71,29 @@ function alertBox(type, text) {
 function alertBox_hide() {
   document.getElementById("alertBox_container").style.visibility = "hidden";
 }
+
+// Si un nouvel utilisateur rejoint le t'chat, on informe le server de son nom
+socket.on("user-joined", (name) => {
+  append(`${name} a rejoint le t'chat`, "right");
+});
+
+// Si le server envoi un message, on le reçoi
+socket.on("receive", (data) => {
+  append(`${data.name}: ${data.message}`, "left");
+});
+
+// Si un utilisateur quitte le t'chat, on informe les autres utilisateurs
+socket.on("left", (name) => {
+  append(`${name} a quitté le t'chat`, "left");
+});
+
+// Si le formulaire est soumi, on envoi le message au server
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  append(`Toi: ${message}`, "right");
+  socket.emit("send", message);
+  messageInput.value = "";
+});
+
+
